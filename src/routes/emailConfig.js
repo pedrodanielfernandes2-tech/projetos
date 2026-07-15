9dcf5ae13ceea380155e8773e3a857e81eb769cd
pedrodanielfernandes2-tech <pedrodanielfernandes2@gmail.com>
2026-07-15 18:17:12 -0300

    1507V45
    Refactor emailConfig routes to use async/await with PostgreSQL pool for database queries.

--- src/routes/emailConfig.js
+++ src/routes/emailConfig.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const db = require('../db');
+const { pool } = require('../db');
 const { sendDigestNow } = require('../email');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(db.prepare('SELECT * FROM email_config WHERE id = 1').get());
+router.get('/', async (req, res) => {
+  const { rows } = await pool.query('SELECT * FROM email_config WHERE id = 1');
+  res.json(rows[0]);
 });
 
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
   const { frequencia, dia_semana, hora, enviar_gps, enviar_admins } = req.body;
-  db.prepare(`
-    UPDATE email_config SET frequencia=?, dia_semana=?, hora=?, enviar_gps=?, enviar_admins=? WHERE id=1
-  `).run(frequencia, dia_semana, hora, enviar_gps ? 1 : 0, enviar_admins ? 1 : 0);
+  await pool.query(`
+    UPDATE email_config SET frequencia=$1, dia_semana=$2, hora=$3, enviar_gps=$4, enviar_admins=$5 WHERE id=1
+  `, [frequencia, dia_semana, hora, !!enviar_gps, !!enviar_admins]);
   res.json({ ok: true });
 });
 
-// dispara o envio agora, ignorando a periodicidade (util para testar)
 router.post('/enviar-agora', async (req, res) => {
   try {
     const resultado = await sendDigestNow();
